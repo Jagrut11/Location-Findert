ecc82348693a998614b08dfc1cfbdbb8cca20321
ManishaUdhani <manisha.mscit17@gmail.com>
2019-02-21 13:39:22 +0530

    Database done

--- database/migrations/2017_11_02_060149_create_users_table.php
+++ database/migrations/2017_11_02_060149_create_users_table.php
@@ -19,12 +19,16 @@ class CreateUsersTable extends Migration
             $table->string('email', 191)->unique();
             $table->string('password', 191)->nullable();
             $table->boolean('status')->default(1);
-            $table->string('confirmation_code', 191)->nullable();
+            $table->string('confirmation_code',191)->nullable();
             $table->boolean('confirmed')->default(0);
-            $table->boolean('is_term_accept')->default(0)->comment(' 0 = not accepted,1 = accepted');
+            $table->boolean('is_term_accept')->default(0)->comment('0 = not accepted,1 = accepted');
             $table->string('remember_token', 100)->nullable();
             $table->integer('created_by')->unsigned();
             $table->integer('updated_by')->unsigned()->nullable();
+            $table->string('designation',191);
+            $table->integer('contact');
+            $table->string('department',191);
+            $table->integer('role_id')->unsigned()->index('users_id_foreign');
             $table->timestamps();
             $table->softDeletes();
         });
---
+++ resources/views/Reg_confirm.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+    <title>Page Title</title>
+    
+</head>
+<body>
+    <div>Let Admin confirm you as a user !</div>
+    <button>OK</button>
+</body>
+</html>
