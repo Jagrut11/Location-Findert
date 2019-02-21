07667b03a722b91cafdba37e7ecf91a4b4e1d9bf
varunshukla097 <varunshukla097@gmail.com>
2019-02-21 14:39:33 +0530

    kai pan

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+    'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 2525),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM', 'viral.solani@gmail.com'),
+        'address' => env('MAIL_FROM', 'jagrutpmodi@gmail.com'),
         'name'    => env('MAIL_FROM_NAME', 'Admin'),
     ],
 
@@ -121,3 +121,5 @@ return [
     ],
 
 ];
+
+
